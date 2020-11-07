5a60458983d7a4fe38fa8e4e160cf1d24b087de7
unknown <adhiragathan12@gmail.com>
2020-11-07 08:59:25 +0700

    initial commit

--- src/if1/pkg10119025/latihan36/targetsaldotabungan/Latihan36TargetSaldoTabungan.java
+++ src/if1/pkg10119025/latihan36/targetsaldotabungan/Latihan36TargetSaldoTabungan.java
@@ -10,6 +10,10 @@ import java.util.Scanner;
 /**
  *
  * @author User
+ * * NAMA     : ADHIRA FAHRI GATHAN
+ * KELAS    : PBO
+ * NIM      : 10119025
+ * Deskripsi Program : Program ini berisi tentang Target Saldo Tabungan
  */
 public class Latihan36TargetSaldoTabungan {
     static Scanner scan = new Scanner(System.in);
